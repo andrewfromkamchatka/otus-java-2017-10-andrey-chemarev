b1930c814c01c8e45216243198fe2f2575232962
andrew <andreychemarev@mail.ru>
2017-11-03 04:04:38 +1100

    Fixed error in get(int index)

--- HW03/src/main/java/ru/chemarev/andrey/MyArrayList.java
+++ HW03/src/main/java/ru/chemarev/andrey/MyArrayList.java
@@ -109,7 +109,7 @@ public class MyArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        if ( index > currentPosition )
+        if ( index >= currentPosition )
             throw new IndexOutOfBoundsException(String.format("Index (%d) is not in the list", index, size()));
 
         return (T) array[index];
