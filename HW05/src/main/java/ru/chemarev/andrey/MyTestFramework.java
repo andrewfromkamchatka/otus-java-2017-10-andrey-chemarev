1b8547282cd344a8d64e63696087ca11cb2ee191
andrew <andreychemarev@mail.ru>
2017-11-17 02:11:43 +1100

    Modified MyTestFramework with considering MyTestClassInfo.

--- HW05/src/main/java/ru/chemarev/andrey/MyTestFramework.java
+++ HW05/src/main/java/ru/chemarev/andrey/MyTestFramework.java
@@ -3,67 +3,79 @@ package ru.chemarev.andrey;
 import com.google.common.reflect.ClassPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.chemarev.andrey.annotation.After;
-import ru.chemarev.andrey.annotation.Before;
-import ru.chemarev.andrey.annotation.Test;
 
 import java.io.IOException;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MyTestFramework {
 
-    public static void testClass(String className) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException {
-        Class clazz = Class.forName(className);
+    private static List<Exception> performTest(Class<?> clazz, Method test, Method before, Method after) {
+        List<Exception> result = new ArrayList<>();
 
-        Method after = null;
-        Method before = null;
-        ArrayList<Method> tests = new ArrayList<>();
-
-        for (Method method : clazz.getDeclaredMethods()) {
-            for (Annotation annotation : method.getDeclaredAnnotations()) {
-
-                if (annotation.annotationType() == After.class) {
-                    if (after == null)
-                        after = method;
-                    else
-                        throw new UnsupportedOperationException(String.format("%s @After more then one", className));
-                }
-
-                else if (annotation.annotationType() == Before.class) {
-                    if (before == null)
-                        before = method;
-                    else
-                        throw new UnsupportedOperationException(String.format("%s @Before more then one", className));
-                }
-
-                else if (annotation.annotationType() == Test.class) {
-                    tests.add(method);
-                }
-
-            }
+        Object object;
+        try {
+            object = clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            result.add(e);
+            return result;
         }
 
-        for (Method test : tests) {
-            logger.trace("Performing {}()", test.getName());
-            Object obj = clazz.newInstance();
+        boolean isTestPossible = true;
+        if ( before != null )
+            isTestPossible = performTestMethod(object, before, result);
 
-            if (after != null)
-                after.invoke(obj);
+        if ( isTestPossible )
+            performTestMethod(object, test, result);
 
-            test.invoke(obj);
+        if ( after != null )
+            performTestMethod(object, after, result);
 
-            if (before != null)
-                before.invoke(obj);
-
-            logger.trace("{}() finished\n", test.getName());
-        }
-
+        return result;
     }
 
-    public static void testPackage(String packageName) throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    private static boolean performTestMethod(Object object, Method method, List<Exception> exceptions) {
+        try {
+            method.invoke(object);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            exceptions.add(e);
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void testClass(String className) throws ClassNotFoundException {
+        Class clazz = Class.forName(className);
+
+        MyTestClassInfo info = MyTestClassInfo.analyze(clazz);
+
+        if ( info.hasErrors() ) {
+            logger.info("{} has syntax errors", clazz.getName());
+            for ( Exception e : info.getErrors() )
+                logger.error( e.getMessage() );
+        } else {
+            for (Method test : info.getTests()) {
+                List<Exception> testErrors = performTest(clazz, test, info.getBefore(), info.getAfter());
+
+                if ( testErrors.size() == 0 )
+                    logger.info("Test {} passed successfully", test.getName());
+                else {
+                    logger.info("Test {} failed", test.getName());
+                    for (Exception e : testErrors) {
+                        if ( e.getClass() == InvocationTargetException.class && e.getCause() != null && e.getCause().getStackTrace().length > 1 )
+                            logger.error("{} {} ", e.getCause().getClass().getName(), e.getCause().getStackTrace()[1]);
+                        else
+                            logger.error("{} {} ", e.getClass().getName(), e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+    public static void testPackage(String packageName) throws IOException, ClassNotFoundException {
         ClassPath classPath = ClassPath.from(ClassLoader.getSystemClassLoader());
         for (ClassPath.ClassInfo classInfo : classPath.getTopLevelClasses(packageName)) {
             testClass(classInfo.getName());
