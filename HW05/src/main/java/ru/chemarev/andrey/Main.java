68782b103d77a038405225410fdebaef2a110562
andrew <andreychemarev@mail.ru>
2017-11-17 02:12:48 +1100

    Small changes in Main class.

--- HW05/src/main/java/ru/chemarev/andrey/Main.java
+++ HW05/src/main/java/ru/chemarev/andrey/Main.java
@@ -1,17 +1,18 @@
 package ru.chemarev.andrey;
 
-import ru.chemarev.andrey.test.TestClass2;
+import ru.chemarev.andrey.test.TestClass1;
+import ru.chemarev.andrey.test.WrongTestClass;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            MyTestFramework.testClass(TestClass2.class.getName());
+//            MyTestFramework.testClass(WrongTestClass.class.getName());
+//            MyTestFramework.testClass(TestClass1.class.getName());
             MyTestFramework.testPackage("ru.chemarev.andrey.test");
-        } catch (ClassNotFoundException | InvocationTargetException | InstantiationException | IllegalAccessException | IOException e) {
+        } catch (ClassNotFoundException | IOException e) {
             e.printStackTrace();
         }
     }
