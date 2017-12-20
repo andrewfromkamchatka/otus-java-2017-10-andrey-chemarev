2865073e20a48e941ae09362a63eeba5b246a28a
andrew <andreychemarev@mail.ru>
2017-12-20 23:30:04 +1100

    Revert "Homework 09"
    This reverts commit 9c24d632d3f0ad6a46e16b285d452a2b174190f1.

--- hw09/src/main/java/ru/chemarev/andrey/core/PreparatoryHandler.java
+++
@@ -1,9 +0,0 @@
-package ru.chemarev.andrey.core;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface PreparatoryHandler {
-    void handle(PreparedStatement statement) throws SQLException;
-}
--- hw09/src/main/java/ru/chemarev/andrey/core/ResultHandler.java
+++
@@ -1,9 +0,0 @@
-package ru.chemarev.andrey.core;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface ResultHandler<T> {
-    T handle(ResultSet resultSet) throws SQLException;
-}
